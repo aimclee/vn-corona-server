8f583798dc3a295bb559971ddb59192bac7b954b
RyanKor <equuse3144@gmail.com>
2020-03-21 12:38:04 +0700

    .

--- corona/urls.py
+++ corona/urls.py
@@ -21,5 +21,5 @@ router = routers.DefaultRouter()
 router.register('virus', NhandanViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('', include(router.urls)),
 ]
