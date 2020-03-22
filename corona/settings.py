da4dc42ef2c2dbe0cd9713e8ee7f5c1898934139
RyanKor <equuse3144@gmail.com>
2020-03-22 12:30:47 +0700

    .

--- corona/settings.py
+++ corona/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0htd*@ke!+h#@l6@a!h*cw_ued%nq+r_n7$uy-6qziu^+no9t3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'corona-dev.eba-ji82nif8.us-west-2.elasticbeanstalk.com', '127.0.0.1']
